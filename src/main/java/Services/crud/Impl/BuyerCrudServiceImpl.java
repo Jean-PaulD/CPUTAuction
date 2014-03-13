dcb82fcad59cca31feff578a70baa6730c6d5c46
Jean-Paul <Jean-Paul@10.0.0.7>
2014-03-13 23:28:55 +0200

    version 1.4

---
+++ src/main/java/Services/crud/Impl/BuyerCrudServiceImpl.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Services.crud.Impl;
+
+import Bid.Bid;
+import Services.crud.BuyerCrudService;
+import java.util.List;
+
+/**
+ *
+ * @author Jean-Paul
+ */
+public class BuyerCrudServiceImpl implements BuyerCrudService{
+
+    //@Override
+   // @Transactional(propagation = Propagation.SUPPORTS)
+    public Bid find(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Bid persist(Bid entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Bid merge(Bid entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Bid remove(Bid entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public List<Bid> findAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
